99483c80b3fd0562fd42964cda42b3f11dfcd033
Marion <lumajimenezla@ittepic.edu.mx>
2025-11-24 00:58:20 -0700

    Subiendo Sección 15 Terminada

--- 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.ts
+++ 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.ts
@@ -1,9 +1,62 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './register-page.html',
 })
-export class RegisterPage { }
+export class RegisterPage {
+  fb = inject(FormBuilder);
+
+  formUtils = FormUtils;
+
+  myForm = this.fb.group(
+    {
+      name: [
+        '',
+        [Validators.required, Validators.pattern(FormUtils.namePattern)],
+      ],
+      email: [
+        '',
+        [Validators.required, Validators.pattern(FormUtils.emailPattern)],
+        [FormUtils.checkingServerResponse],
+      ],
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(FormUtils.notOnlySpacesPattern),
+          FormUtils.notGimmick,
+        ],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', Validators.required],
+    },
+    {
+      validators: [FormUtils.isFieldOneEqualFieldTwo('password', 'password2')],
+    }
+  );
+
+  isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    return (formGroup: AbstractControl) => {
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value === field2Value ? null : { passwordsNotEqual: true };
+    };
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value);
+  }
+}
--- 06-reactive-forms-app/src/app/utils/form-utils.ts
+++ 06-reactive-forms-app/src/app/utils/form-utils.ts
@@ -1,54 +1,121 @@
-import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
+import {
+  AbstractControl,
+  FormArray,
+  FormGroup,
+  ValidationErrors,
+} from '@angular/forms';
+
+async function sleep() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, 2500);
+  });
+}
 
 export class FormUtils {
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
 
-  static getTextError(errors: ValidationErrors){
-
-    for ( const key of Object.keys(errors) ) {
-      switch(key) {
+  static getTextError(errors: ValidationErrors) {
+    for (const key of Object.keys(errors)) {
+      switch (key) {
         case 'required':
           return 'Este campo es requerido';
 
         case 'minlength':
-          return `Mínimo de ${errors['minlength'].requiredLength } caracteres.`;
+          return `Mínimo de ${errors['minlength'].requiredLength} caracteres.`;
 
         case 'min':
-          return `valor mínimo de ${errors['min'].min }`;
+          return `valor mínimo de ${errors['min'].min}`;
+
+        case 'email':
+          return `El valor ingresado no tiene formato de correo`;
+
+        case 'emailTaken':
+          return `El correo electronico ya esta siendo usado por otro usuario`;
           
+        case 'notGimmick':
+          return `No se puede usar ese nombre por aqui amigo mio`;  
+
+        case 'pattern':
+          if (errors['pattern'].requiredPattern === FormUtils.emailPattern) {
+            return `El valor ingresado no luce como un correo electrónico`;
+          }
+
+          return 'Error de patrón contra expresión regular';
+
+        default:
+          return `Error de validación no controlado`;
       }
     }
 
     return null;
-
   }
 
-    static isValidField( form: FormGroup,  fieldName: string ): boolean | null {
-    return( !!form.controls[fieldName].errors && form.controls[fieldName].touched
+  static isValidField(form: FormGroup, fieldName: string): boolean | null {
+    return (
+      !!form.controls[fieldName].errors && form.controls[fieldName].touched
     );
- }
+  }
 
- static getFieldError(form: FormGroup, fieldName: string): string | null {
-
-    if ( !form.controls[fieldName]) return null;
+  static getFieldError(form: FormGroup, fieldName: string): string | null {
+    if (!form.controls[fieldName]) return null;
 
     const errors = form.controls[fieldName].errors ?? {};
 
     return FormUtils.getTextError(errors);
   }
 
-  static isValidFieldInArray( formArray: FormArray, index: number ) {
+  static isValidFieldInArray(formArray: FormArray, index: number) {
     return (
       formArray.controls[index].errors && formArray.controls[index].touched
     );
   }
 
-  static getFieldErrorInArray(formArray: FormArray, index: number): string | null {
+  static getFieldErrorInArray(
+    formArray: FormArray,
+    index: number
+  ): string | null {
     if (formArray.controls.length === 0) return null;
 
     const errors = formArray.controls[index].errors ?? {};
 
     return FormUtils.getTextError(errors);
   }
-}
 
-// FormUtils.isValidField()
+  static isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    return (formGroup: AbstractControl) => {
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value === field2Value ? null : { passwordsNotEqual: true };
+    };
+  }
+
+  static async checkingServerResponse(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> {
+    await sleep();
+
+    const formValue = control.value;
+
+    if (formValue === 'hola@mundo.com') {
+      return {
+        emailTaken: true,
+      };
+    }
+
+    return null;
+  }
+
+  static notGimmick( control: AbstractControl):ValidationErrors | null {
+    const value = control.value;
+    
+    return value === 'Gimmick' ? { notGimmick: true } : null;
+
+
+  } 
+
+}
