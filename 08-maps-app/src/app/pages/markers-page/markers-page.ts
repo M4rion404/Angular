582388c364b153183599b2122297a1ca412f636e
Marion <lumajimenezla@ittepic.edu.mx>
2025-11-27 19:45:47 -0700

    Subiendo Sección 17 Terminada

---
+++ 08-maps-app/src/app/pages/markers-page/markers-page.ts
@@ -0,0 +1,103 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  signal,
+  viewChild,
+} from '@angular/core';
+import mapboxgl, { LngLat, LngLatLike, MapMouseEvent } from 'mapbox-gl';
+import { environment } from '../../../environments/environment';
+import { v4 as UUIDV4 } from 'uuid'
+import { JsonPipe } from '@angular/common';
+
+mapboxgl.accessToken = environment.mapboxKey;
+
+interface Marker {
+  id: string;
+  mapboxMarker: mapboxgl.Marker;
+}
+
+@Component({
+  selector: 'app-markers-page',
+  imports: [JsonPipe],
+  templateUrl: './markers-page.html',
+})
+export class MarkersPage implements AfterViewInit {
+  divElement = viewChild<ElementRef>('map');
+  map = signal<mapboxgl.Map | null>(null);
+  markers = signal<Marker[]>([]);
+
+  async ngAfterViewInit() {
+    if (!this.divElement()?.nativeElement) return;
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    const element = this.divElement()!.nativeElement;
+
+    const map = new mapboxgl.Map({
+      container: element, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-104.8756, 21.4796], // starting position [lng, lat]
+      zoom: 12, // starting zoom
+    });
+
+    // const marker = new mapboxgl.Marker({
+    //   draggable: false,
+    //   color: 'red',
+    // })
+    //   .setLngLat([-104.8756, 21.4796])
+    //   .addTo(map)
+
+    //   marker.on('dragend', (event) =>{
+    //     console.log(event);
+    //   })
+
+    this.mapListeners(map);
+  }
+  mapListeners(map: mapboxgl.Map) {
+    map.on('click', (event) => this.mapClick(event));
+
+    this.map.set(map);
+  }
+  mapClick(event: mapboxgl.MapMouseEvent) {
+    
+    if (!this.map() )return;
+
+    const map = this.map()!;
+    const coords = event.lngLat;
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    console.log(event.lngLat);
+
+    
+    const mapboxMarker = new mapboxgl.Marker({
+      color: color,
+    }).setLngLat(coords)
+      .addTo(map)
+
+      const newMarker: Marker ={
+        id:UUIDV4(),
+        mapboxMarker: mapboxMarker
+      }
+      
+      this.markers.update(markers => [newMarker, ...markers])
+  }
+
+  flyToMarker( lngLat: LngLatLike ) {
+    if ( !this.map() )return;
+
+    this.map()?.flyTo({
+      center: lngLat,
+    });
+  }
+
+  deleteMarker(marker: Marker){
+    if( !this.map() )return
+    const map = this.map()!;
+
+    marker.mapboxMarker.remove();
+
+    this.markers.set(this.markers().filter((m) => m.id != marker.id));
+  }
+}
